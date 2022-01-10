b665eb3992eae9e45072b405df8a127ba8b7a46a
Vladimir Bosic <vldmrbsc@gmail.com>
2022-01-10 15:00:52 +0100

    SignIn and SignUp finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoUri =
   "mongodb+srv://vbosic:vladimirbosic91@mycluster.eq9u1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -10,8 +17,8 @@ mongoose.connect(mongoUri, {
   useUnifiedTopology: true,
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi there !");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email is: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
