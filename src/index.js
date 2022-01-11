60468f261494beaeb650cf3c9a11dc4d03bfd9cb
Vladimir Bosic <vldmrbsc@gmail.com>
2022-01-11 10:13:42 +0100

    TrackAPI finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://vbosic:vladimirbosic91@mycluster.eq9u1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
